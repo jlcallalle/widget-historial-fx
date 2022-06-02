0a413e4bab6b0c3f7da5d506453217403e1c099c
Jorge Callalle <jorge.callalle@vasslatam.com>
2022-06-02 17:05:09 -0500

    se maqueta modulo filtro

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,166 @@
     id="app"
     class="wrapper-widget">
     <div class="widget-historial-fx">
-      <div class="container">
+      <h1 class="mb-4 title-widget">
+        Historial de Operaciones FX
+      </h1>
+      <div class="container container-widget">
         <div class="row">
-          <div class="col-md-12">
-            <p>Template Base</p>
+          <div class="box-consultar">
+            <div class="form-row">
+              <div class="form-group col-md-2">
+                <label for="inputProducto">Producto</label>
+                <select
+                  id="inputProducto"
+                  class="form-control">
+                  <option selected>
+                    FX Spot
+                  </option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                </select>
+              </div>
+              <div class="form-group col-md-2">
+                <label for="inputFecha">Año/Mes</label>
+                <select
+                  id="inputFecha"
+                  class="form-control">
+                  <option selected>
+                    2021 - Febrero
+                  </option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                </select>
+              </div>
+              <div class="form-group col-md-2">
+                <label for="inputEstatus">Estatus General</label>
+                <select
+                  id="inputEstatus"
+                  class="form-control">
+                  <option selected>
+                    FX Spot
+                  </option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                </select>
+              </div>
+              <div class="form-group col-md-2">
+                <label for="inputEstatus">Usuario</label>
+                <select
+                  id="inputEstatus"
+                  class="form-control">
+                  <option selected>
+                    Nombre usuario
+                  </option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                </select>
+              </div>
+              <div class="form-group col-md-2">
+                <label for="inputEstatus">Estatus Pago</label>
+                <select
+                  id="inputEstatus"
+                  class="form-control">
+                  <option selected>
+                    Pendiente Pago
+                  </option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                </select>
+              </div>
+              <div class="form-group col-md-2 box-consulta-descarga">
+                <button
+                  type="submit"
+                  class="btn btn-outline-operacion">
+                  Consultar
+                </button>
+                <div class="icon-descarga">
+                  <svg
+                    width="24"
+                    height="30"
+                    viewBox="0 0 24 30"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M23.9998 10.8333H17.3332V0.833252H7.33317V10.8333H0.666504L12.3332 22.4999L23.9998 10.8333ZM0.666504 25.8333V29.1666H23.9998V25.8333H0.666504Z"
+                      fill="#A41D36" />
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row mt-4">
+          <div class="box-filtrado">
+            <div class="form-row">
+              <div class="form-group col-md-2">
+                <div class="btn btn-pill">
+                  Producto
+                  <button
+                    type="button"
+                    class="close"
+                    aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+              </div>
+              <div class="form-group col-md-2">
+                <div class="btn btn-pill">
+                  Cliente
+                  <button
+                    type="button"
+                    class="close"
+                    aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+              </div>
+              <div class="form-group col-md-2">
+                <div class="btn btn-pill">
+                  Operador
+                  <button
+                    type="button"
+                    class="close"
+                    aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+              </div>
+              <div class="form-group col-md-2">
+                <div class="btn btn-pill">
+                  #Ref/Folio
+                  <button
+                    type="button"
+                    class="close"
+                    aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+              </div>
+
+              <div class="form-group col-md-3">
+                <button
+                  type="submit"
+                  class="btn btn-primary btn-columna">
+                  <i><svg
+                    width="31"
+                    height="24"
+                    viewBox="0 0 31 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M17.9474 6.85714H0V10.2857H17.9474V6.85714ZM17.9474 0H0V3.42857H17.9474V0ZM24.4737 13.7143V6.85714H21.2105V13.7143H14.6842V17.1429H21.2105V24H24.4737V17.1429H31V13.7143H24.4737ZM0 17.1429H11.4211V13.7143H0V17.1429Z"
+                      fill="white" />
+                  </svg>
+                  </i><span>Agregar Columnas</span>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
