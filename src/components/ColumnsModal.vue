afa06bc268268941839b084c326de90b22c1ec3e
Jorge Callalle <jorge.callalle@vasslatam.com>
2022-06-22 10:32:50 -0500

    valida espaciado botones en tablet

--- src/components/ColumnsModal.vue
+++ src/components/ColumnsModal.vue
@@ -7,7 +7,7 @@
         class="modal-wrapper"
         :class="open ? 'slide': ''">
         <div
-          class="modal-dialog modal-dialog-centered modal-dialog-scrollable"
+          class="modal-dialog modal-dialog-centered modal-dialog-scrollable scroll-dialog"
           role="document">
           <div class="modal-content">
             <div class="modal-header">
